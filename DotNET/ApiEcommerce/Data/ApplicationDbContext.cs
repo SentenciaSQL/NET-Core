a3541859c7dcf33a7a226370e560d37b4c5cd573
Andrés Frías <inf512.andres.frias@gmail.com>
2025-11-26 21:34:48 -0400

    ASP,NET Core API

---
+++ DotNET/ApiEcommerce/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using ApiEcommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiEcommerce.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
+        public DbSet<Category> Categories { get; set; }
+    }
+}
